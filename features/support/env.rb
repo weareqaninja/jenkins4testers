d19119df5bd771288bc5e42699502f7dd6c19d68
Fernando Papito <eu@papito.io>
2019-06-10 19:31:40 -0300

    ajustando o driver do chrome para headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -15,6 +15,15 @@ when "firefox"
 when "chrome"
   @driver = :selenium_chrome
 when "headless"
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    chrome_options =  Selenium::WebDriver::Chrome::Options.new.tap do |options|
+      options.add_argument "--headless"
+      options.add_argument "--disable-gpu"
+      options.add_argument "--no-sandbox"
+      options.add_argument "--disable-site-isolation-trials"
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+  end
   @driver = :selenium_chrome_headless
 else
   puts "Invalid browser"
