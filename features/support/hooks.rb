bead869b00db0bb14eba02a59bce6eda14f8920a
Fernando Papito <eu@papito.io>
2019-06-11 19:05:59 -0300

    trabalhando com screenshots

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -12,3 +12,7 @@ Before("@login") do
   @login_page.with(user["email"], user["pass"])
 end
 
+After do |scenario|
+  screenshot = page.save_screenshot("log/screenshots/#{scenario.__id__}.png")
+  embed(screenshot, "image/png", "Screenshot")
+end
