0e5a7366c1b16ff1f5bc232e49839ba7f4cc4244
Papito <eu@papito.io>
2020-08-04 18:15:21 -0300

    Update helpers.rb

--- features/support/helpers.rb
+++ features/support/helpers.rb
@@ -3,7 +3,7 @@
 module Helpers
   def get_token
     timeout = Capybara.default_max_wait_time
-    2.times do
+    timeout.times do
       js_script = 'return window.localStorage.getItem("default_auth_token");'
       @token = page.execute_script(js_script)
       break if @token != nil
